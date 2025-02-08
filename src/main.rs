92169f24716126938ca19500bff2f70a37c2a908
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-02-08 16:07:47 +0600

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use fancy::printcoln;
-use std::{env, process::exit, time::Instant};
+use std::{env, path::Path, process::exit, time::Instant};
 
 fn time_took(start: Instant) {
     printcoln!("took: [green]{:.2?}\n", start.elapsed());
@@ -7,6 +7,7 @@ fn time_took(start: Instant) {
 }
 
 fn success_message(file_name: &String) {
+    print!("🚀 ");
     printcoln!(
         "[green]PROCESS:[:] Successfully created [yellow|italic]`{}.rs[:]`!",
         file_name
@@ -22,6 +23,15 @@ fn main() {
     }
 
     let file_name = &args[1];
+    let cargo_config = Path::new("Cargo.toml");
+
+    if !cargo_config.exists() {
+        printcoln!(
+            "Missing [yellow|italic]`Cargo.toml`[:] in \n[blue]{}[:]",
+            args[0]
+        );
+        time_took(start);
+    }
 
     if file_name.ends_with(".rs") {
         printcoln!("  Enter the file name without '.rs' extension!\n [italic]e.g.,[:] [:blue]'my_file'[:] instead of [blue]'my_file.rs'");
