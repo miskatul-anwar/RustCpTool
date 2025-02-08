2173ccc954ee502c95f2763cd071e1757767a19a
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-02-08 15:56:04 +0600

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
-use core::time;
-use std::thread::sleep;
+use fancy::printcoln;
+use std::{env, process::exit, time::Instant};
 
-use cliclack::{confirm, progress_bar};
+fn time_took(start: Instant) {
+    printcoln!("took: [green]{:.2?}\n", start.elapsed());
+    exit(1);
+}
 
+fn success_message(file_name: &String) {
+    printcoln!(
+        "[green]PROCESS:[:] Successfully created [yellow|italic]`{}.rs[:]`!",
+        file_name
+    );
+}
 fn main() {
-    let _should_continue = confirm("Do you want to continue?").interact();
-    let progress = progress_bar(100);
-    progress.start("Installation...");
-    for _ in 1..=5 {
-        sleep(time::Duration::from_millis(1000));
-        progress.inc(20);
+    let args: Vec<String> = env::args().collect();
+    let start = Instant::now();
+
+    if args.len() < 2 {
+        printcoln!("[red]  No file name provided!\n [green] create_rs <file_name>");
+        time_took(start);
     }
-    progress.stop("Installation Complete");
+
+    let file_name = &args[1];
+
+    if file_name.ends_with(".rs") {
+        printcoln!("  Enter the file name without '.rs' extension!\n [italic]e.g.,[:] [:blue]'my_file'[:] instead of [blue]'my_file.rs'");
+        time_took(start);
+    }
+
+    println!("Processing file: {}", file_name);
+    success_message(file_name);
 }
